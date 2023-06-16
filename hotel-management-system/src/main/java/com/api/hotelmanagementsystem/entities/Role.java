72191cc1ba22b44e3aac3ff2fe1159a9a6aa626e
Pedro Crespan <pedrocrespan@hotmail.com>
2023-06-16 10:46:43 -0300

    Added role validation

--- hotel-management-system/src/main/java/com/api/hotelmanagementsystem/entities/Role.java
+++ hotel-management-system/src/main/java/com/api/hotelmanagementsystem/entities/Role.java
@@ -3,6 +3,8 @@ package com.api.hotelmanagementsystem.entities;
 import com.api.hotelmanagementsystem.entities.enums.RoleName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
 import java.util.List;
@@ -16,7 +18,11 @@ public class Role implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Name can't be blank")
     private String name;
+
+    @NotNull(message = "Role name can't be null")
     private RoleName roleName;
     @OneToMany(mappedBy = "role")
     @JsonIgnore
